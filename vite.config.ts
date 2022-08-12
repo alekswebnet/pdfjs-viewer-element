816708ea76fc4d58522aec1f4e6d07e0b1e3dc05
alekswebnet <alekswebnet@gmail.com>
2022-08-12 06:52:03 +0300

    Fix publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: 'src/pdfjs-viewer-element.ts',
-      formats: ['es']
+      formats: ['es'],
     },
+    manifest: true,
     rollupOptions: {
-      external: /^lit/
-    }
-  }
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 })
