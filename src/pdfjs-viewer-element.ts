5387adac205a4dfd4e79ca288eeebf2072379ca5
Oleksandr Shevchuk <34628096+alekswebnet@users.noreply.github.com>
2023-08-10 16:45:24 +0300

    Update PDF.js prebuilt version, fix viewer rerendering

--- src/pdfjs-viewer-element.ts
+++ src/pdfjs-viewer-element.ts
@@ -1,4 +1,5 @@
 import { elementReady } from "./elementReady";
+import { debounce } from 'perfect-debounce'
 
 const DEFAULTS = {
   viewerPath: '/pdfjs',
@@ -7,8 +8,8 @@ const DEFAULTS = {
   page: '',
   search: '',
   phrase: '',
-  zoom: '',
-  pagemode: '',
+  zoom: 'auto',
+  pagemode: 'none',
   locale: '',
   textLayer: ''
 }
@@ -38,10 +39,18 @@ export class PdfjsViewerElement extends HTMLElement {
   }
 
   attributeChangedCallback() {
-    elementReady('iframe', this.shadowRoot!).then(() => this.setProps())
+    this.debouncedRenderViewer()
   }
 
-  private setProps() {
+  private debouncedRenderViewer = debounce(async () => {
+    await elementReady('iframe', this.shadowRoot!)
+    const updatedSrc = this.getIframeSrc()
+    if (updatedSrc !== this.iframe.getAttribute('src')) {
+      this.renderViewer(updatedSrc)
+    }
+  }, 0, { leading: true })
+
+  private getIframeSrc() {
     const src = this.getFullPath(this.getAttribute('src') || DEFAULTS.src)
     const viewerPath = this.getFullPath(this.getAttribute('viewer-path') || DEFAULTS.viewerPath)
     const page = this.getAttribute('page') || DEFAULTS.page
@@ -52,24 +61,32 @@ export class PdfjsViewerElement extends HTMLElement {
     const locale = this.getAttribute('locale') || DEFAULTS.locale
     const textLayer = this.getAttribute('text-layer') || DEFAULTS.textLayer
 
-    const updatedSrc = `${viewerPath}${DEFAULTS.viewerEntry}?file=${src}#page=${page}&zoom=${zoom}&pagemode=${pagemode}&search=${search}&phrase=${phrase}&textLayer=${textLayer}${locale ? '&locale='+locale : ''}`
-    if (updatedSrc !== this.iframe.getAttribute('src')) {
-      this.rerenderIframe(updatedSrc)
-    }
+    return `${viewerPath}${DEFAULTS.viewerEntry
+      }?file=${src
+      }#page=${page
+      }&zoom=${zoom
+      }&pagemode=${pagemode
+      }&search=${search
+      }&phrase=${phrase
+      }&textLayer=${textLayer}${
+        locale ? '&locale='+locale : ''
+    }`
   }
 
-  private rerenderIframe(src: string) {
+  private renderViewer(src: string) {
     this.shadowRoot!.replaceChild(this.iframe.cloneNode(), this.iframe)
     this.iframe = this.shadowRoot!.querySelector('iframe') as PdfjsViewerElementIframe
     this.iframe.contentWindow.location.href = src
+    console.log('ok')
   }
 
   private setEventListeners() {
     document.addEventListener('webviewerloaded', () => {
-      if (this.getAttribute('src') !== DEFAULTS.src) this.iframe.contentWindow.PDFViewerApplicationOptions.set('defaultUrl', '')
-      this.iframe.contentWindow.PDFViewerApplicationOptions.set('disablePreferences', true)
-      this.iframe.contentWindow.PDFViewerApplicationOptions.set('pdfBugEnabled', true)
-    })
+      if (this.getAttribute('src') !== DEFAULTS.src) this.iframe.contentWindow.PDFViewerApplicationOptions?.set('defaultUrl', '')
+      this.iframe.contentWindow.PDFViewerApplicationOptions?.set('disablePreferences', true);
+      this.iframe.contentWindow.PDFViewerApplicationOptions?.set('pdfBugEnabled', true);
+      this.iframe.contentWindow.PDFViewerApplicationOptions?.set('eventBusDispatchToDOM', true);
+    });
   }
 
   private getFullPath(path: string) {
@@ -87,6 +104,7 @@ export interface PdfjsViewerElementIframeWindow extends Window {
   PDFViewerApplicationOptions: {
     set: (name: string, value: string | boolean) => void
   }
+  PDFViewerApplication: any
 }
 
 export interface PdfjsViewerElementIframe extends HTMLIFrameElement {
